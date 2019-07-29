8c82622df6b5f3c4b9b9ce43c10c739d73e0452b
SteveK1964 <steve.kuplic@comcast.net>
2019-07-29 13:14:48 -0500

    Committed .gitignore

--- routes/auth.js
+++ routes/auth.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', name: 'Steve'});
+  res.render('auth/index', { title: 'User Authentication' });
 });
 
 module.exports = router;
